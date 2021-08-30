551ffd4d14e4f7acbc92d023e2137f4c11b68932
Ihar Zapolskj <alonemoutain@gmail.com>
2021-08-30 20:25:29 +0300

    end

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
-const slides = document.querySelectorAll('.slide');
 
-for(const slide of slides){
-    slide.addEventListener('click', () => {
-        clearActiveClasses()
+function slidesPlugin(activeSlide = 0){
 
-        slide.classList.add('active')
+    const slides = document.querySelectorAll('.slide');
 
-        if(slide.classList.contains('desert')){
-            document.body.style.backgroundColor="#5a5833"
-        }else if(slide.classList.contains('vulcano')){
-            document.body.style.backgroundColor="#2a6974"
-        }else if(slide.classList.contains('sea')){
-            document.body.style.backgroundColor="#15310d"
-        }else if(slide.classList.contains('moutain')){
-            document.body.style.backgroundColor="#6093b6"
-        }else{
-            document.body.style.backgroundColor="#111"
-        }
-    })
+    slides[activeSlide].classList.add('active')
+
+    for(const slide of slides){
+        slide.addEventListener('click', () => {
+            clearActiveClasses()
+
+            slide.classList.add('active')
+
+            if(slide.classList.contains('desert')){
+                document.body.style.backgroundColor="#5a5833"
+            }else if(slide.classList.contains('vulcano')){
+                document.body.style.backgroundColor="#2a6974"
+            }else if(slide.classList.contains('sea')){
+                document.body.style.backgroundColor="#15310d"
+            }else if(slide.classList.contains('moutain')){
+                document.body.style.backgroundColor="#6093b6"
+            }else{
+                document.body.style.backgroundColor="#111"
+            }
+        })
+    }
+
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        }) 
+    }
+
 }
 
-function clearActiveClasses() {
-    slides.forEach((slide) => {
-        slide.classList.remove('active')
-    }) 
-}
-
-
-
+slidesPlugin(3)
